77add2e1f3ef7aad17368288f3117e5cda98fc82
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-20 22:47:42 +0330

    layout created for auth pages.

---
+++ layouts/auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main class="primary">
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'AuthLayout'
+}
+</script>
