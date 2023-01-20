15e6f8ca0d81d89628b19bc4fff79996debc05cf
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-20 23:45:00 +0330

    login page implemented.
    login page created with a form and validation checking for text fields.

---
+++ pages/login/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container>
+    <v-snackbar
+      v-model="showSnackbar"
+      color="grey darken-3"
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
+    <v-row justify="center">
+      <v-col cols="7" align-self="center">
+        <v-card class="px-12 py-6">
+          <v-card-title>
+            Login with username and password
+          </v-card-title>
+          
+          <v-form
+            ref="form"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="username"
+              label="username"
+              :rules="basicRules"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              label="password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              counter
+              @click:append="showPassword = !showPassword"
+              :rules="basicRules"
+              required
+            ></v-text-field>
+          </v-form>
+
+          <v-card-actions>
+            <v-btn class="primary" @click.prevent="loginSubmit()">
+              Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'LoginPage',
+  layout: 'auth',
+  data: () => ({
+    username: '',
+    password: '',
+    showPassword: false,
+    showSnackbar: false,
+    snackbarMessage: '',
+    basicRules: [
+      v => !!v || 'Field is required'
+    ]
+  }),
+  methods: {
+    ...mapActions('auth', ['login']),
+
+    loginSubmit () {
+      if (this.$refs.form.validate()) {
+        this.login().then(() => {
+          this.$router.push('/')
+        }).catch((msg) => {
+          this.snackbarMessage = msg
+          this.showSnackbar = true
+        })
+      }
+    }
+  }
+}
+</script>
