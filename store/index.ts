828c546c5ff445d4b66bea612a42873cd3f30dda
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-20 22:44:00 +0330

    base store file created

---
+++ store/index.ts
@@ -0,0 +1,17 @@
+import { GetterTree, MutationTree, ActionTree } from "vuex"
+
+export interface RootState {
+}
+
+export const state = (): RootState => ({
+})
+
+export const getters: GetterTree<RootState, RootState> = {
+}
+
+export const mutations: MutationTree<RootState> = {
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+}
+
