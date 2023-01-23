12993f851d511788160f71672dd359bc740c35ae
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-23 23:23:22 +0330

    exam table implemented

---
+++ components/exam_dates_table.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <div class="text-h6 py-4">Exam Date Times</div>
+    <v-data-table
+      :headers="headers"
+      :items="examDates"
+      class="elevation-1"
+    ></v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ExamDatesTable",
+  props: {
+    examDates: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    headers: [
+      { text: "ID", value: "course_id" },
+      { text: "Course Name", value: "course_name" },
+      { text: "Date", value: "exam_date" },
+      { text: "Start Time", value: "start_at" },
+      { text: "Finish Time", value: "end_at" },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
