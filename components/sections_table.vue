df53518200e5fded62bb79f39200622936992b5e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-23 10:52:51 +0330

    section table colors changed. hover added. bug fixed on hover and push router to section page

--- components/sections_table.vue
+++ components/sections_table.vue
@@ -1,26 +1,32 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left" v-for="header in headers" :key="header.value">
-            {{ header.text }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in table_data"
-          :key="item.course_id"
-          @click.prevent="this.$router.push(`section/${item.id}/`)"
-        >
-          <td>{{ item.course_id }}</td>
-          <td>{{ item.course_name }}</td>
-          <td>{{ item.credit }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-container>
+    <div class="text-h5 py-4 font-weight-bold ">Active Courses</div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead class="primary">
+          <tr>
+            <th class="text-left white--text" v-for="header in headers" :key="header.value">
+              {{ header.text }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in sections"
+            :key="item.id"
+            class="blue lighten-4"
+            @click.prevent="$router.push(`section/${item.id}/`)"
+          >
+            <td>{{ item.course_id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.credit }}</td>
+            <td>{{ item.department }}</td>
+            <td>{{ item.class_room }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
@@ -37,24 +43,16 @@ export default {
       {text: 'ID', value: 'course_id'},
       {text: 'Course Name', value: 'course_name'},
       {text: 'Credit', value: 'credit'},
+      {text: 'Department', value: 'department'},
+      {text: 'Class Room', value: 'class_room'},
     ]
   }),
-  computed: {
-    table_data() {
-      arr = []
-      this.sections.forEach(element => {
-        arr.push(this.table_row(element))
-      });
-      return arr
-    },
-    table_row(section) {
-      return {
-        id: section.id,
-        course_id: section.course.id,
-        credit: section.course.credit,
-        course_name: section.course.name
-      }
-    }
-  }
 }
 </script>
+
+<style scoped>
+tr:hover {
+  color: red;
+  cursor: pointer;
+}
+</style>
