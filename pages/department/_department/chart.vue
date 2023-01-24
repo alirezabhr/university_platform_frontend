58ed916fd2c8747a63987565b91c507839bd3a1c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-24 18:01:13 +0330

    chart page implemented

---
+++ pages/department/_department/chart.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-col>
+    <v-row v-for="(course, index) in chart" :key="index">
+        <div>Course Suggested Term: {{ course.suggested_term }}</div>
+        <div>Course ID: {{ course.course_id }}</div>
+        <div>Course Name: {{ course.course_name }}</div>
+        <div>Credits: {{ course.credit }}</div>
+        <div>Prerequiesite: {{ course.pishniaz_course_term }}</div>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+    name: 'DepartmentChartPage',
+    mounted() {
+        this.getChart()
+    },
+    data: () => ({
+        chart: []
+    }),
+    methods: {
+        getChart() {
+            const url = process.env.baseURL + `department/${this.$route.params.department}/chart/`
+
+            this.$axios.get(url).then((resp) => {
+                this.chart = resp.data
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
