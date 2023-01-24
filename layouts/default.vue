664b00ce8b976c1194391908ac4753d04c14c603
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2023-01-24 11:39:05 +0330

    home and logout button added

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,38 @@
       fixed
       app
     >
-      <v-toolbar-title>{{ userPositionTitle }}</v-toolbar-title>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+          >
+            {{ userPositionTitle }}
+            <v-icon>mdi-menu-down</v-icon>
+          </v-btn>
+
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in getUserPositions"
+            :key="index"
+          >
+            <v-list-item-title>
+              <v-btn @click.prevent="changeUserPosition(getUserPositions[index])" text>{{ item.title }}</v-btn>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
       <v-spacer/>
+      <v-btn
+        icon
+        @click.prevent="$router.push('/')"
+        class="px-5"
+      >
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
       <v-btn
         outlined
         @click.prevent="logout"
@@ -14,7 +44,7 @@
         Logout
       </v-btn>
     </v-app-bar>
-    <v-main>
+    <v-main class="blue lighten-5">
       <v-container>
         <Nuxt />
       </v-container>
@@ -23,7 +53,7 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapMutations, mapGetters } from 'vuex'
 
 export default {
   name: 'DefaultLayout',
@@ -35,9 +65,14 @@ export default {
   },
   methods: {
     ...mapActions('auth', ['logout']),
+    ...mapMutations('auth', ['setUserPosition']),
+
+    changeUserPosition(position) {
+      this.setUserPosition(position)
+    }
   },
   computed: {
-    ...mapGetters('auth', ['getUserCurrenctPosition']),
+    ...mapGetters('auth', ['getUserCurrenctPosition', 'getUserPositions']),
 
     userPositionTitle () {
       if (this.getUserCurrenctPosition) {
